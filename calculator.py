26ae9431e69d6a838a21d2e508667eef5726419c
zerotosystems <zerotosystems23@gmail.com>
2025-12-22 03:30:39 +0400

    Day 2: variables and data types - basic calculation with python

---
+++ calculator.py
@@ -0,0 +1,7 @@
+a = float(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Addition:",a + b)
+print("Subtraction:",a - b)
+print("Multiplication:",a * b)
+print("Division:",a / b)
---
+++ variable.py
@@ -0,0 +1,9 @@
+age = 23
+height = 5.6
+name = "Zero to Systems"
+is_learning = True
+
+print(age)
+print(height)
+print(name)
+print(is_learning)
